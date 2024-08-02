073fd02a6cdc72182e1860b5d3c1134495469c86
Mr_Hamun <said.elatik.602@gmail.com>
2024-08-03 01:02:02 +0300

    added tailwind and firebase

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#071952',
+        secondary: '#F97316'
+      }
+    },
+  },
+  plugins: [],
+}
